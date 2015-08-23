659b251ed0920dc033481f56c4f0cd9f56a2a625
Brett Viren <brett.viren@gmail.com>
2015-08-22 22:03:27 -0400

    Building but fails at run time not findging libstdc++.

--- pkgs/development/libraries/physics/rootsys/default.nix
+++ pkgs/development/libraries/physics/rootsys/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, cmake }:
+{ stdenv, fetchurl, cmake, gcc, zlib, freetype, pcre, xz, x11, libXpm, giflib, libtiff, libjpeg_turbo, gsl, python, mesa, graphviz, libxml2, openssl, mysql, postgresql, sqlite, pkgconfig, fftw, doxygen }:
 
 stdenv.mkDerivation rec {
   name = "rootsys-${version}";
@@ -9,7 +9,31 @@ stdenv.mkDerivation rec {
     sha256 = "16irxlpl5xirz4v5mnnfs672j6v1j21lmf4xjrjzabjrllvmwhc1";
   };
 
+  enableParallelBuilding = true;
 
+  # existence of configure hides cmake
+  preConfigure = ''
+    rm -f configure
+  '';
+
+  buildInputs = [ cmake gcc zlib freetype pcre xz x11 libXpm giflib libtiff libjpeg_turbo gsl python mesa graphviz libxml2 openssl mysql postgresql sqlite pkgconfig fftw doxygen ];
+
+  cmakeFlags = ''
+  -Dcxx11=ON
+  -Dbuiltin_xrootd=ON
+  -Dminuit2=ON
+  -Dpython=ON
+  -Dfftw3=ON
+  -Dgdml=ON
+  -Dgviz=ON
+  -DMYSQL_CONFIG_EXECUTABLE="{mysql}/bin/mysql_config"
+  '';
+
+  meta = {
+    homepage = "http://root.cern.ch/";
+    description = "A data analysis framework";
+    platforms = stdenv.lib.platforms.linux;
+  };
   
 }
   
